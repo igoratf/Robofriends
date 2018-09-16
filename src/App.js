0bff8e097e30665351d7fc26e4856b865c59000a
igoratf <igor.atf@gmail.com>
2018-09-16 20:38:49 -0300

    add robot search filter

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+        
+    
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        console.log(filteredRobots);
+
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
             </div>
         );
     }
