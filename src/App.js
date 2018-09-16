4edd0ee4225677463edf42af60e2fcd6cc876e9d
igoratf <igor.atf@gmail.com>
2018-09-16 20:22:06 -0300

    change App render syntax to default

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-export class App extends Component {
+class App extends Component {
     render() {
         return (
             <div className="tc">
