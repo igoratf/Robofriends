66ffb45af22240c0fa2253180d54945ee61e46ec
igoratf <igor.atf@gmail.com>
2018-09-16 18:17:46 -0300

    render cardlist with map

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = ({ name, email, id }) => {
     return (
-        <div class="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?200x200`}/>
             <div>
                 <h2>{name}</h2>
--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    const cardsArray = robots.map((robot, i) => {
+        return (
+        <Card
+         key={i}
+         id={robot.id}
+         name={robot.name} 
+         email={robot.email}
+         />
+        );
+    })
     return (
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+            {cardsArray}
         </div>
     );
 }
