47bddca8ab9f6659eeed9b1dc3d3d70545926db8
igoratf <igor.atf@gmail.com>
2018-09-16 20:21:03 -0300

    add searchbox and change app render to default

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import CardList from './CardList';
-import { robots } from './robots';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-                <CardList robots={robots}/>
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
