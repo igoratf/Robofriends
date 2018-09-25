d32b6bf05cd604822d1410f54834be8a6f96fe97
igoratf <igor.atf@gmail.com>
2018-09-24 23:55:07 -0300

    add loading message while robots are not available

--- src/App.js
+++ src/App.js
@@ -14,12 +14,8 @@ class App extends Component {
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => {
-            return response.json();
-        })
-        .then(users => {
-            this.setState({ robots : users})
-        })
+        .then(response => response.json())
+        .then(users => this.setState({ robots : users}));
     }
 
     onSearchChange = (event) => {
@@ -32,13 +28,17 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className="f1">Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
