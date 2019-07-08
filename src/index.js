b936c187c2c91b124a1fe9a10ce97a53de172ffb
Igor Farias <igor.farias@ccc.ufcg.edu.br>
2019-07-08 10:58:46 -0300

    add async redux actions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'; 
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { rootReducer } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger))
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger)) // middlewares are ordered
 
 ReactDOM.render(
    <Provider store={store}>
